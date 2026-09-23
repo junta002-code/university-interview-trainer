80e92a597b3b5f249bdaec0b8b331f8257bfba9a
junta002-code <junta002@gmail.com>
2026-09-23 21:06:38 +0900

    変更を反映

--- sw.js
+++ sw.js
@@ -11,12 +11,14 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
+      self.skipWaiting();
     })
   );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
+    self.clients.claim();
     caches.keys().then(keys => {
       return Promise.all(
         keys
